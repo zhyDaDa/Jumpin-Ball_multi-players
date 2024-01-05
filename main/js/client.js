d44183ef22a804bcdd08f73b420b601cf6c65976
zhyDaDa <zhyDaDa_nightmare@outlook.com>
2024-01-05 14:06:12 +0800

    视图可调整缩放 视图中心点修正, 以及相机偏移量修正 窗口大小缩放自动调整缩放

--- main/js/client.js
+++ main/js/client.js
@@ -57,7 +57,7 @@ var Clarity = function() {
     this.alert_errors = false;
     this.log_info = true;
     this.tile_size = 16;
-    this.limit_viewport = false;
+    this.limit_viewport = true;
     this.jump_switch = 0;
     this.dash_switch = 0;
 
@@ -353,289 +353,6 @@ Clarity.prototype.draw_map = function(context) {
 
 };
 
-Clarity.prototype.move_player = function() {
-
-    var tX = this.player.loc.x + this.player.vel.x;
-    var tY = this.player.loc.y + this.player.vel.y;
-
-    var offset = Math.round((this.tile_size / 2) - 1);
-
-    var tile = this.get_tile(
-        Math.round(this.player.loc.x / this.tile_size),
-        Math.round(this.player.loc.y / this.tile_size)
-    );
-
-    if (tile.gravity) {
-
-        this.player.vel.x += tile.gravity.x;
-        this.player.vel.y += tile.gravity.y;
-
-    } else {
-
-        this.player.vel.x += this.current_map.gravity.x;
-        this.player.vel.y += this.current_map.gravity.y;
-    }
-
-    if (tile.friction) {
-
-        this.player.vel.x *= tile.friction.x;
-        this.player.vel.y *= tile.friction.y;
-    }
-
-    var t_y_up = Math.floor(tY / this.tile_size);
-    var t_y_down = Math.ceil(tY / this.tile_size);
-    var y_near1 = Math.round((this.player.loc.y - offset) / this.tile_size);
-    var y_near2 = Math.round((this.player.loc.y + offset) / this.tile_size);
-
-    var t_x_left = Math.floor(tX / this.tile_size);
-    var t_x_right = Math.ceil(tX / this.tile_size);
-    var x_near1 = Math.round((this.player.loc.x - offset) / this.tile_size);
-    var x_near2 = Math.round((this.player.loc.x + offset) / this.tile_size);
-
-    var top1 = this.get_tile(x_near1, t_y_up);
-    var top2 = this.get_tile(x_near2, t_y_up);
-    var bottom1 = this.get_tile(x_near1, t_y_down);
-    var bottom2 = this.get_tile(x_near2, t_y_down);
-    var left1 = this.get_tile(t_x_left, y_near1);
-    var left2 = this.get_tile(t_x_left, y_near2);
-    var left3 = this.get_tile(t_x_left - 1, y_near1);
-    var left4 = this.get_tile(t_x_left - 1, y_near2);
-    var right1 = this.get_tile(t_x_right, y_near1);
-    var right2 = this.get_tile(t_x_right, y_near2);
-    var right3 = this.get_tile(t_x_right + 1, y_near1);
-    var right4 = this.get_tile(t_x_right + 1, y_near2);
-
-
-    if (tile.jump && this.jump_switch > 15) {
-
-        this.player.can_jump = true;
-        this.player.doublejumpFlag = false;
-        this.player.can_doublejump = true;
-
-        this.player.can_dash = true;
-
-        this.jump_switch = 0;
-
-    } else this.jump_switch++;
-
-    this.player.vel.x = Math.min(Math.max(this.player.vel.x, -this.current_map.vel_limit.x), this.current_map.vel_limit.x);
-    this.player.vel.y = Math.min(Math.max(this.player.vel.y, -this.current_map.vel_limit.y), this.current_map.vel_limit.y);
-
-    /* dash技能处理 */
-    if (left1.solid || left2.solid || right1.solid || right2.solid || left3.solid || left4.solid || right3.solid || right4.solid) { this.dash_switch = 0; }
-    if (this.dash_switch > 0) {
-        this.player.vel.x = this.key.left ? -10 : 10;
-        this.dash_switch--;
-    }
-
-    /* float技能处理 */
-    if (this.player.float_ability && this.player.can_float && this.key.key_k == true) {
-        for (var p = -3; p < 3; p++) {
-            for (var q = -2; q < 4; q++) {
-                var localTile = this.get_tile(t_x_left + q, t_y_up + p);
-                if (localTile.solid) { continue; }
-                if (!(this.current_map.data[t_y_up + p] && this.current_map.data[t_y_up + p][t_x_left + q])) { continue; }
-                this.current_map.data[t_y_up + p][t_x_left + q] = deepCopy({ colour: '#EADBC5', solid: 0, gravity: { x: 0, y: -0.3 } });
-                //colour: '#EADBC5', solid: 0, gravity: { x: 0, y: -0.3 }
-            }
-        }
-        this.player.can_float = false;
-        this.player.can_dash = true;
-        this.player.can_jump = true;
-        this.player.can_doublejump = true;
-        this.player.doublejumpFlag = false;
-    }
-
-    if (this.player.loc.y > this.current_map.height_p + 100) {
-        // player坠落死亡
-    }
-
-    // h6_1.innerHTML = this.player.loc.x/this.tile_size;
-    // h6_2.innerHTML = this.player.loc.y/this.tile_size;
-    h6_3.innerHTML = "死亡计次: " + this.player.deaths;
-
-
-    this.player.loc.x += this.player.vel.x;
-    this.player.loc.y += this.player.vel.y;
-
-    this.player.vel.x *= .9;
-
-    if (left1.solid || left2.solid || right1.solid || right2.solid) {
-
-        /* 解决重叠 */
-
-        while (this.get_tile(Math.floor(this.player.loc.x / this.tile_size), y_near1).solid ||
-            this.get_tile(Math.floor(this.player.loc.x / this.tile_size), y_near2).solid)
-            this.player.loc.x += 0.1;
-
-        while (this.get_tile(Math.ceil(this.player.loc.x / this.tile_size), y_near1).solid ||
-            this.get_tile(Math.ceil(this.player.loc.x / this.tile_size), y_near2).solid)
-            this.player.loc.x -= 0.1;
-
-        /* 瓷砖反弹 */
-
-        var bounce = 0;
-
-        if (left1.solid && left1.bounce > bounce) bounce = left1.bounce;
-        if (left2.solid && left2.bounce > bounce) bounce = left2.bounce;
-        if (right1.solid && right1.bounce > bounce) bounce = right1.bounce;
-        if (right2.solid && right2.bounce > bounce) bounce = right2.bounce;
-
-        this.player.vel.x *= -bounce || 0;
-
-    }
-
-    if (top1.solid || top2.solid || bottom1.solid || bottom2.solid) {
-
-        /* 解决重叠 */
-
-        while (this.get_tile(x_near1, Math.floor(this.player.loc.y / this.tile_size)).solid ||
-            this.get_tile(x_near2, Math.floor(this.player.loc.y / this.tile_size)).solid)
-            this.player.loc.y += 0.1;
-
-        while (this.get_tile(x_near1, Math.ceil(this.player.loc.y / this.tile_size)).solid ||
-            this.get_tile(x_near2, Math.ceil(this.player.loc.y / this.tile_size)).solid)
-            this.player.loc.y -= 0.1;
-
-        /* 瓷砖反弹 */
-
-        var bounce = 0;
-
-        if (top1.solid && top1.bounce > bounce) bounce = top1.bounce;
-        if (top2.solid && top2.bounce > bounce) bounce = top2.bounce;
-        if (bottom1.solid && bottom1.bounce > bounce) bounce = bottom1.bounce;
-        if (bottom2.solid && bottom2.bounce > bounce) bounce = bottom2.bounce;
-
-        this.player.vel.y *= -bounce || 0;
-
-        if ((bottom1.solid || bottom2.solid) && !tile.jump) {
-
-            this.player.on_floor = true;
-            this.player.can_jump = true;
-            this.player.can_doublejump = true;
-            this.player.doublejumpFlag = false;
-
-            this.player.can_dash = true;
-
-        }
-
-    }
-
-    // 调整相机
-    // 基于player的loc和viewport的调整相机的位置
-    {
-        var c_x = Math.round(this.player.loc.x - this.viewport.x / 2);
-        var c_y = Math.round(this.player.loc.y - this.viewport.y / 2);
-        var x_dif = Math.abs(c_x - this.camera.x);
-        var y_dif = Math.abs(c_y - this.camera.y);
-
-        if (x_dif > 8) {
-
-            var mag = Math.round(Math.max(1, x_dif * 0.1));
-
-            if (c_x != this.camera.x) {
-
-                this.camera.x += c_x > this.camera.x ? mag : -mag;
-
-                if (this.limit_viewport) {
-
-                    this.camera.x =
-                        Math.min(
-                            this.current_map.width_p - this.viewport.x + this.tile_size,
-                            this.camera.x
-                        );
-
-                    this.camera.x =
-                        Math.max(
-                            0,
-                            this.camera.x
-                        );
-                }
-            }
-        }
-
-        if (y_dif > 8) {
-
-            var mag = Math.round(Math.max(1, y_dif * 0.1));
-
-            if (c_y != this.camera.y) {
-
-                this.camera.y += c_y > this.camera.y ? mag : -mag;
-
-                if (this.limit_viewport) {
-
-                    this.camera.y =
-                        Math.min(
-                            this.current_map.height_p - this.viewport.y + this.tile_size,
-                            this.camera.y
-                        );
-
-                    this.camera.y =
-                        Math.max(
-                            0,
-                            this.camera.y
-                        );
-                }
-            }
-        }
-    }
-
-    if (this.last_tile != tile.id && tile.script) {
-        // 如果当前的tile与上一个不同, 则执行这个tile的脚本
-        eval(this.current_map.scripts[tile.script]);
-    }
-
-    this.last_tile = tile.id;
-
-    // 如果当前地图有trap, 则执行
-    if (typeof this.current_map.trap === "function") this.current_map.trap();
-};
-
-Clarity.prototype.update_player = function() {
-
-    if (this.key.left) {
-
-        if (this.player.vel.x > -this.current_map.vel_limit.x)
-            this.player.vel.x -= this.current_map.movement_speed.left;
-    }
-    if (this.key.up) {
-        if (this.player.can_jump && !this.player.doublejumpFlag && this.player.can_doublejump && this.player.vel.y > -this.current_map.vel_limit.y) {
-
-            this.player.vel.y -= this.current_map.movement_speed.jump;
-            this.player.can_jump = false;
-
-            this.jump_switch = 0;
-        }
-        if (this.player.doublejump_ability && !this.player.can_jump && this.player.doublejumpFlag && this.player.can_doublejump && this.player.vel.y > -this.current_map.vel_limit.y && this.jump_switch > 20) {
-
-            this.player.vel.y *= 0.8;
-            this.player.vel.y -= this.current_map.movement_speed.jump;
-            this.player.can_doublejump = false;
-        }
-    } else {
-        if (!this.player.can_jump && !this.player.doublejumpFlag && this.player.can_doublejump || this.player.can_jump && this.player.doublejumpFlag && this.player.can_doublejump) this.player.doublejumpFlag = true;
-        this.jump_switch++;
-    }
-    if (this.key.right) {
-
-        if (this.player.vel.x < this.current_map.vel_limit.x)
-            this.player.vel.x += this.current_map.movement_speed.left;
-    }
-    if (this.key.down && this.player.glide_ability) {
-
-        this.player.vel.y *= 0.8;
-    }
-    if (this.key.key_j && this.player.dash_ability && this.player.can_dash) {
-        this.player.can_dash = false;
-        this.dash_switch = 5;
-    }
-    // if (this.key.key_k && this.player.float_ability && this.player.can_float) {
-    //     this.player.can_float = false;
-    // }
-
-    this.move_player();
-};
-
 Clarity.prototype.draw_all_players = function(context, players) {
     for (let player of players)
         this.draw_player(context, player);
@@ -689,8 +406,8 @@ Clarity.prototype.update = function() {
 };
 
 Clarity.prototype.update_camera = function(target_x, target_y) {
-    var c_x = Math.round(target_x - this.viewport.x / 2);
-    var c_y = Math.round(target_y - this.viewport.y / 2);
+    var c_x = Math.round(target_x - this.viewport.x / 2 + this.tile_size / 2);
+    var c_y = Math.round(target_y - this.viewport.y / 2 + this.tile_size / 2);
     var x_dif = Math.abs(c_x - this.camera.x);
     var y_dif = Math.abs(c_y - this.camera.y);
 
@@ -761,6 +478,18 @@ Clarity.prototype.draw = function(context, map_id, players, camera) {
 
     // 调整相机位置
     this.update_camera(camera.x, camera.y);
+
+    // 画出相机
+    // context.fillStyle = '#f00';
+    // context.fillRect(
+    //     // Math.round(camera.x - this.viewport.x / 2 + this.tile_size) / zoomIndex,
+    //     // Math.round(camera.y - this.viewport.y / 2 + this.tile_size) / zoomIndex,
+    //     (this.viewport.x / 2),
+    //     (this.viewport.y / 2),
+    //     16,
+    //     16
+    // );
+    // console.log(this.viewport);
 };
 
 function setSkinColor(color) {
@@ -778,17 +507,17 @@ function setPlayerName(name) {
 const canvas = document.getElementById('canvas'),
     ctx = canvas.getContext('2d');
 
+const game = new Clarity();
+
 // 获取窗口宽高, 并设置canvas的大小
 function setViewZoom(zoomIndex) {
     const w = window.innerWidth,
         h = window.innerHeight;
     canvas.width = w / zoomIndex;
     canvas.height = h / zoomIndex;
+    game.set_viewport(canvas.width, canvas.height);
 }
 setViewZoom(zoomIndex);
-
-const game = new Clarity();
-game.set_viewport(canvas.width, canvas.height);
 
 const tipBoard = document.getElementById("tipBoard");
 const playerColour = '#FF9900';
